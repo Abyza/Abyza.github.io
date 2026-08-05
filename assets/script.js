12d2ae50a5aaeca567dca358b2445b146fe3e7aa
Abyza <paulomendoza1432@gmail.com>
2026-08-06 07:55:32 +0800

    fix youtube display

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,15 @@
-function loadYouTubeVideo(element, videoId) {
-  element.innerHTML = `
+function loadYouTubeVideo(container, videoId) {
+  // Prevent any parent link triggers
+  if (window.event) window.event.stopPropagation();
+
+  container.innerHTML = `
     <iframe 
-      width="100%" 
-      height="100%" 
-      src="https://www.youtube.com/embed/${videoId}?autoplay=1" 
+      src="https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0" 
       title="YouTube video player" 
       frameborder="0" 
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-      allowfullscreen>
-    </iframe>
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      allowfullscreen
+      style="width: 100%; height: 100%; position: absolute; top: 0; left: 0; border: none; border-radius: inherit;"
+    ></iframe>
   `;
 }
