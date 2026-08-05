ed1d87be21c41a0e8d11f8e74175ddf51c539c9f
Abyza <paulomendoza1432@gmail.com>
2026-08-06 07:51:56 +0800

    test new video style

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+function loadYouTubeVideo(element, videoId) {
+  element.innerHTML = `
+    <iframe 
+      width="100%" 
+      height="100%" 
+      src="https://www.youtube.com/embed/${videoId}?autoplay=1" 
+      title="YouTube video player" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      allowfullscreen>
+    </iframe>
+  `;
+}
